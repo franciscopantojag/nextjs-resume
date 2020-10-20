f362c77bf5ee0284bff0c33f940bc1009211c0ae
franciscopantojag <franciscopantojag98@gmail.com>
2020-10-20 04:00:11 -0500

    testing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import Head from "next/head";
 import initReactFastclick from "react-fastclick";
-// initReactFastclick();
+initReactFastclick();
 function MyApp({ Component, pageProps }) {
   return (
     <>
