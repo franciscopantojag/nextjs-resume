a8182296d0c36a0257916720583a373f6d547d88
Francisco Pantoja <franciscopantojag98@gmail.com>
2023-10-23 19:33:43 -0500

    chore: update job experience

--- src/sections/experience.js
+++ src/sections/experience.js
@@ -11,10 +11,33 @@ export default function Experience() {
       <div className="cardContainer">
         <div className="card">
           <div className="cardContent">
+          <div className="cardCont">
+              <h5>
+                <span>
+                  <FontAwesomeIcon icon={faCalendarAlt} /> Jun 2023 - Present
+                </span>
+              </h5>
+              <h3>Software Engineer</h3>
+              <h4>BairesDev</h4>
+              <ul>
+                <li>
+                  <span className="colorRed">- </span>
+                  <span>GraphQL Development</span>
+                </li>
+                <li>
+                  <span className="colorRed">- </span>{' '}
+                  <span>REST API Development</span>
+                </li>
+                <li>
+                  <span className="colorRed">- </span>{' '}
+                  <span>Unit Testing</span>
+                </li>
+              </ul>
+            </div>
             <div className="cardCont">
               <h5>
                 <span>
-                  <FontAwesomeIcon icon={faCalendarAlt} /> 2021 - Present
+                  <FontAwesomeIcon icon={faCalendarAlt} /> Feb 2021 - Jun 2023
                 </span>
               </h5>
               <h3>Software Engineer</h3>
@@ -34,29 +57,7 @@ export default function Experience() {
                 </li>
               </ul>
             </div>
-            <div className="cardCont">
-              <h5>
-                <span>
-                  <FontAwesomeIcon icon={faCalendarAlt} /> 2019
-                </span>
-              </h5>
-              <h3>Quality Assurance Assistant</h3>
-              <h4>KAR{' & '}MA S.A.C.</h4>
-              <ul>
-                <li>
-                  <span className="colorRed">- </span>
-                  <span>Product quality assurance and control</span>
-                </li>
-                <li>
-                  <span className="colorRed">- </span>{' '}
-                  <span>Supply chain management</span>
-                </li>
-                <li>
-                  <span className="colorRed">- </span>{' '}
-                  <span>Value chain analysis</span>
-                </li>
-              </ul>
-            </div>
+           
           </div>
         </div>
       </div>
